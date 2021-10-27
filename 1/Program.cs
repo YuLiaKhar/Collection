3a83b763361f563ab905df87bcc6423fdf08087e
YuLiaKhar <julia.harchenko02@gmail.com>
2021-10-27 17:32:50 +0300

    new

---
+++ 1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "([]{})[]";
+            //string b = "([]]";
+
+            Stack<char> line = new Stack<char>();
+            bool k, l, m, n;
+            k = l = m = true;
+            for (int i = 0; i < a.Length; i++)
+            {
+                char c = a[i];
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    line.Push(c);
+                }
+
+                if (c == ')')
+                {
+                    if (line.Contains('('))
+                    {
+                        line.Pop();
+                        k = true;
+                    }
+                    else
+                    {
+                        k = false;
+                    }
+                }
+                if (c==']')
+                {
+                    if (line.Contains('['))
+                    {
+                        line.Pop();
+                        l = true;
+                    }
+                    else
+                    {
+                        l = false;
+                    }
+                }
+                if (c=='}')
+                {
+                    if (line.Contains('{'))
+                    {
+                        line.Pop();
+                        m = true;
+                    }
+                    else
+                    {
+                        m = false;
+                    }
+                }
+
+            }
+            if ((k == true) && (l == true) && (m == true))
+            {
+                Console.WriteLine("Корректно");
+            }
+            else
+            {
+                Console.WriteLine("Некорректно");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static int BracketCheck(string s)
+        {
+            string t = "[{(]})";
+            Stack<char> st = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int f = t.IndexOf(s[i]);
+
+                if (f >= 0 && f <= 2)
+                    st.Push(s[i]);
+
+                if (f > 2)
+                {
+                    if (st.Count == 0 || st.Pop() != t[f - 3])
+                        return i;
+                }
+            }
+
+            if (st.Count != 0)
+                return s.LastIndexOf(st.Pop());
+
+            return -1;
+        }
+
+        static void Main()
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(BracketCheck(s) == -1 ? "Success" : BracketCheck(s).ToString());
+            Console.ReadKey();
+        }
+    }
+}
